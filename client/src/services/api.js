a2f254dff517c3a6a5e6656d2ccdd3c18f330c10
Stutityagi-18 <stutityagi18@gmail.com>
2026-06-17 14:00:34 +0530

    updated api url for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://mannverse.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
